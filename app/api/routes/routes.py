5a4523ce1999ae8b792611ef334e356eccba4615
al7senn <al7senn@gmail.com>
2025-04-09 13:04:24 +0300

    Update routes.py

--- app/api/routes/routes.py
+++ app/api/routes/routes.py
@@ -8,6 +8,7 @@ from app.bot.utils import TelegramError
 from app.services.cryptopay import CryptoPayError
 from app.services.db import DataBase, DatabaseError
 from app.services.payment_successful import payment_success
+
 import logging
 
 class Handlers:
@@ -19,7 +20,12 @@ class Handlers:
     async def payments_webhook(self, request: Request) -> PlainTextResponse:
         try:
             validated_data = PaymentsRequestModel(** await request.json())
-            await payment_success(self.bot, self.database, validated_data.update_type, validated_data.payload.invoice_id)
+            await payment_success(
+                self.bot,
+                self.database,
+                validated_data.update_type,
+                validated_data.payload.invoice_id
+            )
             return PlainTextResponse('OK', status_code=200)
         except ValidationError:
             return PlainTextResponse('Wrong request', status_code=400)
@@ -33,17 +39,10 @@ class Handlers:
 
     async def bot_webhook(self, request: Request) -> JSONResponse:
         try:
-            update = types.Update(**await request.json())
-            await self.dp.feed_webhook_update(self.bot, update)
-            return JSONResponse(content={"status": "ok"})
-        except ValueError:
-            return JSONResponse(content={"message": "Wrong request"}, status_code=400)
-        except DatabaseError:
-            return JSONResponse(content={"message": "database error"}, status_code=500)
-        except CryptoPayError:
-            return JSONResponse(content={"message": "cryptopay error"}, status_code=500)
-        except TelegramError:
-            return JSONResponse(content={"message": "telegram error"}, status_code=500)
+            data = await request.json()
+            update = types.Update(**data)
+            await self.dp.feed_update(self.bot, update)
+            return JSONResponse({"status": "ok"})
         except Exception as e:
             logging.exception(e)
-            return JSONResponse(content={"message": "error"}, status_code=500)
+            return JSONResponse({"status": "error", "detail": str(e)}, status_code=500)
